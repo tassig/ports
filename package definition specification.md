9596e2a6a9413e324fc482e548519a6f00675207
dummy <dummy>
2016-04-19 12:39:23 +0100

    more packages

---
+++ package definition specification.md
@@ -0,0 +1,33 @@
+# Packages management facilities specifications
+
+## Package identifiers
+
+Each package is identified by a unique identifier, which is the filename of the package definition in the directory `packages/`
+Examples: an package unique identifier can be `strace` or `autoconf`
+
+
+## Package definition
+
+A package definition is a file in the directory `packages/`
+
+### Local, optional variables
+
+These variables are defined to be used internally within the package functions (if they exist) or by the function `defaultbuild()`. They MUST NOT be used anywhere else.
+
+`package_name`: internal name used by the package. NB: This is *not* the unique identifier of the package
+`package_version`
+`tarball_suffix`
+`url`
+
+### Public, optional variables
+
+`iscustombuild`: `0` or `1`, if `1` then a function `custombuild()` needs to be provided, such function will be called in place of the `defaultbuild()` function
+`haspostinstall`: `0` or `1`, if `1` then a function `haspostinstall()` needs to be provided, such function will be called after the `defaultbuild()` has completed
+
+### Public, mandatory variables
+
+These variables are mandatory and can be used by without restrictions, by internal or external scripts.
+
+`build_dependencies`: a list of packages unique identifiers separated by spaces, representing the packages needed by this package at build time. Empty if there are no build build_dependencies
+`runtime_dependencies`: same as above, but representing the packages needed at runtime
+
--- packageinstall.sh
+++ packageinstall.sh
@@ -2,41 +2,42 @@
 
 # the function called by default to build a package, works for most packages
 defaultbuild(){
-        package_fullname=$package_name-$package_version
-        wget $url
-        tar xvf $package_fullname.tar.$tarball_suffix
-        cd $package_fullname/
-        ./configure --prefix=/opt/$package_fullname/
-        make
-        make install
-        ln -s /opt/$package_fullname /opt/$package_name
-        ln -s /opt/$package_name/bin/* /bin/
-        cd ..
+	package_fullname=$package_name-$package_version
+	wget $url
+	tar xvf $package_fullname.tar.$tarball_suffix
+	cd $package_fullname/
+	./configure --prefix=/opt/$package_fullname/
+	make
+	make install
+	ln -s /opt/$package_fullname /opt/$package_name
+	ln -s /opt/$package_name/bin/* /bin/
+	cd ..
 }
 
 
 installpackage(){
-        echo "installing package $1"
-
-        source $1
-        echo "dependencies of $1: $build_dependencies"
-
-        # installing the dependencies recursively
-        for pkg_name in $build_dependencies
-        do
-                (installpackage $pkg_name)
-        done
-
-        # do a custom build if the package defines custombuild(), otherwise do a default build
-        if test iscustombuild
-        then custombuild
-        else defaultbuild
-        fi
-        
-        if test haspostinstall
-        then postinstall
-        fi
-        
+	echo "installing package $1"
+	
+	source $1
+	echo "dependencies of $1: $build_dependencies"
+	
+	# installing the dependencies recursively
+	for pkg_name in $build_dependencies
+	do
+		(installpackage $pkg_name)
+	done
+	
+	# do a custom build if the package defines custombuild(), otherwise do a default build
+	if test iscustombuild
+	then custombuild
+	else defaultbuild
+	fi
+	
+	# call its postinstall() function if the package defines postinstall(), otherwise do nothing
+	if test haspostinstall
+	then postinstall
+	fi
+	
 }
 
 
