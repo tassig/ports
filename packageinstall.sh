452ecba324faa8890e3a77d78b31896af14b9648
Dambaev Alexander <ice.redmine@gmail.com>
2016-08-15 18:38:19 +0800

    packageinstall.sh: 80-column aligned

--- packageinstall.sh
+++ packageinstall.sh
@@ -2,11 +2,15 @@
 
 # usage: ./packageinstall.sh package_identifier
 # example: ./packageinstall.sh autoconf
-# the script is assuming you're running it in the ports directory (which is a bit stupid, need to be changed later)
-# refer to "package definition specifications.md" for laws and regulations
+# the script is assuming you're running it in the ports directory (which is a 
+# bit stupid, need to be changed later) refer to "package definition 
+# specifications.md" for laws and regulations
 #
-# TODO: installpackage() should be a transaction to prevent half-installed packages and leftovers. ex: what do you do when "make install" returns an error? right now, we do nothing, which is incorrect
-# TODO: make all url relative to mirrors.tassig.com. dambaev: added support of relative urls.
+# TODO: installpackage() should be a transaction to prevent half-installed 
+#   packages and leftovers. ex: what do you do when "make install" returns an 
+#   error? right now, we do nothing, which is incorrect
+# TODO: make all url relative to mirrors.tassig.com. dambaev: added support of 
+#   relative urls.
 
 # version 1.1
 #
@@ -18,11 +22,14 @@
 
 set -e
 
-packagedirectory=packages   # the directory name where the packages definitions are located
+packagedirectory=packages   # the directory name where the packages definitions
+                            # are located
 
-installdirectory="/opt"   # TODO: implement per user installs (with different --prefix)
+installdirectory="/opt"   # TODO: implement per user installs (with different 
+                          # --prefix)
 
-mirror_prefix=http://mirrors.tassig.com # this is for relative URLs. If $url is empty, then 
+mirror_prefix=http://mirrors.tassig.com # this is for relative URLs. If $url is
+                                        # empty, then 
                                         # url=$mirror_prefix/$rel_url
 
 # the function called by default to build a package, works for most packages
@@ -31,7 +38,8 @@ defaultbuild(){
     mkdir -p builddir   # do everything in builddir for tidiness
     cd builddir
     if [ "$url" == "" ]; then
-        # if no absolute $url defined, then assume, that we have relative url $rel_url
+        # if no absolute $url defined, then assume, that we have relative url 
+        # $rel_url
         url=$mirror_prefix/$rel_url
     fi
     wget -O archive $url
@@ -40,14 +48,17 @@ defaultbuild(){
     cd *   # cd into the package directory
     ./configure --prefix=$installdirectory/$package_fullname/
     make -j
-    if test -z $no_check   # run the make check, unless $no_check is set for this package definition
+    if test -z $no_check   # run the make check, unless $no_check is set for 
+                           # this package definition
     then make -j check || make -j test
     fi
     make install
     ln -sv $installdirectory/$package_fullname $installdirectory/$package_name
-    ln -sv $installdirectory/$package_name/bin/* /bin/ || true   # don't crash if the links are already there
+    ln -sv $installdirectory/$package_name/bin/* /bin/ || true   # don't crash 
+                                            # if the links are already there
     if [ -d "$installdirectory/$package_name/lib/pkgconfig" ]; then
-        ln -svf $installdirectory/$package_name/lib/pkgconfig/* $installdirectory/pkgconf/lib/pkgconfig/   # install pkg-config files
+        ln -svf $installdirectory/$package_name/lib/pkgconfig/* \
+        $installdirectory/pkgconf/lib/pkgconfig/   # install pkg-config files
     fi
     cd ../..
     rm -r builddir
@@ -65,23 +76,28 @@ installpackage(){
     package_fullname=$package_name-$package_version 
     if [ "$rel_url" == "" ]; then
         # if there is no defined rel_url, then define it by default
-        rel_url=$package_name/$package_fullname.tar.$tarball_suffix # default URL, relative to $mirror_prefix
+        rel_url=$package_name/$package_fullname.tar.$tarball_suffix # default 
+                                            # URL, relative to $mirror_prefix
     fi
     echo "build dependencies of $1: $build_dependencies"
 	
 	# installing the package's dependencies recursively
 	for pkg_name in $build_dependencies
 	do
-		(./packageinstall.sh $pkg_name) || exit $?   # i initially used a recursive call to "installpackage" instead, but it was inheriting the shell variables
+		(./packageinstall.sh $pkg_name) || exit $?   # i initially used a 
+                    # recursive call to "installpackage" instead, but it 
+                    # was inheriting the shell variables
 	done
 	
-	# do a custom build if the package defines custombuild(), otherwise do a default build
+	# do a custom build if the package defines custombuild(), otherwise do a 
+    # default build
 	if test $iscustombuild
 	then custombuild
 	else defaultbuild
 	fi
 	
-	# call its postinstall() function if the package defines postinstall(), otherwise do nothing
+	# call its postinstall() function if the package defines postinstall(), 
+    # otherwise do nothing
 	if test $haspostinstall
 	then postinstall
 	fi
