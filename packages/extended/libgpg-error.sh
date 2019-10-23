87feb262c915e5f881933e4fc7e2f9cd42e79fbe
Dimitrije Dinic <dinic.dimitrije@gmail.com>
2019-10-23 11:24:36 +0000

    libgpg-error added to extended

---
+++ packages/extended/libgpg-error.sh
@@ -0,0 +1,4 @@
+package_name=libgpg-error
+package_version=1.36
+tarball_suffix=bz2
+build_dependencies=
--- packages/extended/libgpgerror.sh
+++
@@ -1,3 +0,0 @@
-
-# url: https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.36.tar.bz2
-# NOTE: build went smoothly, install pkg-config files and binaries
