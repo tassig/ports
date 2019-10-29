09ef1c33c4a25d1701c21c1c8289ddcae9de14e4
ivan <Axiom>
2019-10-29 09:34:21 +0000

    meson, ninja

---
+++ packages/extended/meson.sh
@@ -0,0 +1,7 @@
+
+# TODO: depends on python3-better, so can't really include this package
+
+pip3 install meson
+
+# TODO: then need to symlink python bin directory again, that's where meson is
+
---
+++ packages/extended/ninja.sh
@@ -0,0 +1,10 @@
+
+# pre-requisites: 
+# - python (i'm using python3-better from ports.git)
+#
+
+
+# download: https://github.com/ninja-build/ninja/archive/v1.8.2/ninja-1.8.2.tar.gz
+# run: python configure.py --bootstrap
+
+# then copy ninja somewhere so it's available in the path
