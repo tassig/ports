a62f4e75b1ab39032ddb77791f944b2e309a31e6
Dimitrije Dinic <dinic.dimitrije@gmail.com>
2019-10-29 15:36:07 +0000

    appstream-glib: 0.7.15 installation with meson and ninja

--- packages/extended/appstream-glib.sh
+++ packages/extended/appstream-glib.sh
@@ -1,16 +1,9 @@
 package_name=appstream-glib
-package_version=0.7.16
+package_version=0.7.15
 tarball_suffix=xz
-build_dependencies="glib gdk-pixbuf libarchive libsoup"
+build_dependencies="glib gdk-pixbuf libarchive libsoup meson ninja"
 no_check=1 
 
-# We use old version 0.1.7, the last one that builds with gtk2, all newer versions require gtk3
-# TODO: no, i tried latest versions and disabled a few things in meson_options.txt , and it doesn't require gtk3
-# TODO: error during linking, needs -PIC library from e2fsprogs
-
-# We use custom build just because --enable-introspection=no, with default build we get error:
-# Couldn't find include 'GdkPixbuf-2.0.gir'\
-# TODO: solution is to add libgirepository1.0, so we can have clean default build
 
 custombuild(){
 	rm -rf builddir
@@ -21,15 +14,21 @@ custombuild(){
 	rm archive
 	cd *   # cd into the package directory
 
-	./configure --prefix=$installdirectory/$package_fullname --enable-introspection=no
+	mkdir build
+	cd build
 
-	make -j
-	if test -z $no_check   # run the make check, unless $no_check is set for 
-						   # this package definition
-	then make -j check || make -j test
-	fi
+	# NOTE: for now we disable many options, out of following reasons:
+	#    -Ddep11=false - produce ERROR: Dependency "yaml-0.1" not found
+	#    -Dbuild=false  - produce ERROR: Dependency "gdk-3.0" not found
+	#    -Drpm=false - produce  ERROR: Dependency "rpm" not found
+	#    -Dstemmer=false - produce ERROR: C library 'stemmer' not found
+	#    -Dman=false - produce I/O error : Attempt to load network entit http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl
+	#    -Dintrospection=false - produce Couldn't find include 'GdkPixbuf-2.0.gir' TODO: we need libgir?
+	meson .. --prefix=$installdirectory/$package_fullname -Ddep11=false -Dbuilder=false -Drpm=false -Dstemmer=false -Dman=false -Dintrospection=false
 
-	make install
+	ninja
+
+	meson install
 
 	ln -sv $installdirectory/$package_fullname $installdirectory/$package_name
 	ln -sv $installdirectory/$package_name/bin/* /bin/ || true
