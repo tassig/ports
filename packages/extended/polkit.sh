fa1c4dbe0700ee6bcdd2d22a6f99e4b2054ac4c6
Dimitrije Dinic <dinic.dimitrije@gmail.com>
2019-10-30 17:10:24 +0000

    polkit-0.116

--- packages/extended/polkit.sh
+++ packages/extended/polkit.sh
@@ -1,10 +1,11 @@
 package_name=polkit
-package_version=0.98
+package_version=0.116
 tarball_suffix=gz
 build_dependencies="Linux-PAM gobject-introspection expat"
 no_check=1 
 
 custombuild(){
+	SRC_DIR=$(pwd)
 	rm -rf builddir
 	mkdir -p builddir   # do everything in builddir for tidiness
 	cd builddir
@@ -13,15 +14,19 @@ custombuild(){
 	rm archive
 	cd *   # cd into the package directory
 
+	# Apply alpine patch
+	patch -p1 < $SRC_DIR/packages/extended/polkit/make-innetgr-optional.patch
 
-	# TODO: we still get non fatal error message:
-	# Error loading shared library libnettle.so.6: No such file or directory (needed by /opt/glib-2.48.1/lib/gio/modules/libgiognutls.so)
-	# Failed to load module: /opt/glib-2.48.1/lib/gio/modules/libgiognutls.so
-
+	# Apply patch to fix issue with perl script (src/polkitbackend/toarray.pl), which fails to update cpp file
+	# TODO: although it compiles, not sure if this is proper fix, so fix perl script instead
+	sed -i 's:/usr/bin/perl:/bin/perl:' src/polkitbackend/toarray.pl
+	#sed -i 's:init_js:"init.js":g' src/polkitbackend/polkitbackendjsauthority.cpp
 
 	export CPPFLAGS="-I/opt/expat/include -I/opt/Linux-PAM/include"
 	export LDFLAGS="-L/opt/expat/lib -L/opt/Linux-PAM/lib"
-	./configure --prefix=$installdirectory/$package_fullname --disable-man-pages
+
+	# TODO: resolve issue with g-ir-scanner, we had to use --enable-introspection=no temporarly
+	./configure --prefix=$installdirectory/$package_fullname --disable-libelogind --enable-introspection=no --disable-man-pages
 
 	make -j
 	if test -z $no_check   # run the make check, unless $no_check is set for 
