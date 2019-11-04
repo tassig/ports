e9643217076cc04df19de6d0f0955948cbdf60c7
Dambaev Alexander <ice.redmine@gmail.com>
2019-11-04 07:52:14 +0000

    patchelf: initial commit of utility, that can modify executable's interpreter

---
+++ packages/extended/patchelf.sh
@@ -0,0 +1,33 @@
+
+package_name=patchelf
+package_version=0.10
+tarball_suffix=gz
+build_dependencies=""
+no_check=1
+
+function custombuild(){
+	builddir="builddir-$package_fullname"  # build directory is "builddir" followed by the name of the package, which allows multiple builds of different software in parallel
+	rm -rf $builddir
+	mkdir -p $builddir   # do everything in builddir for tidiness
+	cd $builddir
+	wget -O archive $url
+	tar xvf archive
+	rm archive
+	cd *   # cd into the package directory
+  ./bootstrap.sh
+	./configure --prefix=$installdirectory/$package_fullname/
+	ncpu=`cat /proc/cpuinfo | grep processor | wc -l`
+	make -j$ncpu
+	if test -z $no_check   # run the make check, unless $no_check is set for this package definition
+	then make -j$ncpu check || make -j$ncpu test
+	fi
+	make install
+	ln -snv $installdirectory/$package_fullname $installdirectory/$package_name || true
+	ln -sv $installdirectory/$package_name/bin/* $bindirectory || true   # don't crash if the links are already there
+	if [ -d "$installdirectory/$package_fullname/lib/pkgconfig" ]; then
+		ln -svf $installdirectory/$package_fullname/lib/pkgconfig/* $installdirectory/pkgconf/lib/pkgconfig/   # symlink pkg-config files
+	fi
+	# NOTE: .m4 files should probably be installed as well by default, but i have no strong opinion on it, so for now we leave this to the postinstall() function
+	cd ../..
+	rm -r $builddir
+}
