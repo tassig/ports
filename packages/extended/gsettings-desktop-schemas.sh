ddbe3131f8f8d8f47d98126e2d4dbfc8fd383569
Dimitrije Dinic <dinic.dimitrije@gmail.com>
2019-11-04 11:47:05 +0000

    gsettings-desktop-schemas: comment

--- packages/extended/gsettings-desktop-schemas.sh
+++ packages/extended/gsettings-desktop-schemas.sh
@@ -2,3 +2,6 @@ package_name=gsettings-desktop-schemas
 package_version=3.20.0
 tarball_suffix=xz
 build_dependencies="glib"
+
+# IMPORTANT NOTE: after re-installing gsettings-desktop-schemas, we need to reintall metacity.sh and metacity-theme.sh (both in hutchr/applications)
+#                 usually we don't need to reinstall it, it comes with default axiom image
