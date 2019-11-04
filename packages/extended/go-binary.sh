8f00382ea16c975cc0a927c467c4c0f272382589
Dambaev Alexander <ice.redmine@gmail.com>
2019-11-04 07:53:35 +0000

    go-binary: initial commit of prebuilt go compiler

---
+++ packages/extended/go-binary.sh
@@ -0,0 +1,24 @@
+
+package_name=go
+package_version=1.13.4.linux-amd
+tarball_suffix=gz
+build_dependencies="patchelf"
+
+function custombuild(){
+	  builddir="builddir-$package_fullname"  # build directory is "builddir" followed by the name of the package, which allows multiple builds of different software in parallel
+	  rm -rf $builddir
+	  mkdir -p $builddir   # do everything in builddir for tidiness
+	  cd $builddir
+	  wget -O archive $url
+	  tar xvf archive
+	  rm archive
+	  cd *   # cd into the package directory
+
+    # patching built executable to use MUSL LIBC instead of glibc
+    patchelf --set-interpreter /lib/ld-musl-x86_64.so.1 bin/go
+    mkdir -p $installdirectory/$package_fullname
+    cp -r * $installdirectory/$package_fullname
+
+	  cd ../..
+	  rm -r $builddir
+}
